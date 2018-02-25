be2e07ea8b11937a11ffe7365d9cecbd6668663a
JuanCGar <JuanCGar@users.noreply.github.com>
2018-02-25 16:28:30 -0600

    adding all the microoperation to the simulator

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,75 +29,27 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ejecutarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.salirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.button1 = new System.Windows.Forms.Button();
+            this.BT_Princ = new System.Windows.Forms.Button();
+            this.IRBox = new System.Windows.Forms.RichTextBox();
+            this.PCBox = new System.Windows.Forms.RichTextBox();
+            this.MDRBox = new System.Windows.Forms.RichTextBox();
+            this.MARBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.menuStrip1.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.ACBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.archivoToolStripMenuItem,
-            this.editarToolStripMenuItem,
-            this.ejecutarToolStripMenuItem,
-            this.salirToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(704, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // archivoToolStripMenuItem
-            // 
-            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
-            this.archivoToolStripMenuItem.Text = "Archivo";
-            this.archivoToolStripMenuItem.Click += new System.EventHandler(this.archivoToolStripMenuItem_Click);
-            // 
-            // editarToolStripMenuItem
-            // 
-            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
-            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
-            this.editarToolStripMenuItem.Text = "Editar";
-            this.editarToolStripMenuItem.Click += new System.EventHandler(this.editarToolStripMenuItem_Click);
-            // 
-            // ejecutarToolStripMenuItem
-            // 
-            this.ejecutarToolStripMenuItem.Name = "ejecutarToolStripMenuItem";
-            this.ejecutarToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.ejecutarToolStripMenuItem.Text = "Ejecutar";
-            this.ejecutarToolStripMenuItem.Click += new System.EventHandler(this.ejecutarToolStripMenuItem_Click);
-            // 
-            // salirToolStripMenuItem
-            // 
-            this.salirToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.salirToolStripMenuItem1});
-            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
-            this.salirToolStripMenuItem.Text = "Salir";
-            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
-            // 
-            // salirToolStripMenuItem1
-            // 
-            this.salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
-            this.salirToolStripMenuItem1.Size = new System.Drawing.Size(96, 22);
-            this.salirToolStripMenuItem1.Text = "Salir";
             // 
             // richTextBox1
             // 
             this.richTextBox1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(354, 57);
+            this.richTextBox1.Location = new System.Drawing.Point(428, 59);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(338, 455);
             this.richTextBox1.TabIndex = 2;
@@ -110,79 +62,169 @@
             this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
-            // button1
+            // BT_Princ
             // 
-            this.button1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(364, 518);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(316, 40);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Enter";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BT_Princ.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_Princ.Location = new System.Drawing.Point(450, 545);
+            this.BT_Princ.Name = "BT_Princ";
+            this.BT_Princ.Size = new System.Drawing.Size(316, 40);
+            this.BT_Princ.TabIndex = 4;
+            this.BT_Princ.Text = "Enter";
+            this.BT_Princ.UseVisualStyleBackColor = true;
+            this.BT_Princ.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // IRBox
+            // 
+            this.IRBox.Location = new System.Drawing.Point(788, 83);
+            this.IRBox.Name = "IRBox";
+            this.IRBox.Size = new System.Drawing.Size(65, 31);
+            this.IRBox.TabIndex = 2;
+            this.IRBox.Text = "";
+            this.IRBox.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // PCBox
+            // 
+            this.PCBox.Location = new System.Drawing.Point(788, 133);
+            this.PCBox.Name = "PCBox";
+            this.PCBox.Size = new System.Drawing.Size(65, 31);
+            this.PCBox.TabIndex = 2;
+            this.PCBox.Text = "";
+            this.PCBox.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // MDRBox
+            // 
+            this.MDRBox.Location = new System.Drawing.Point(788, 183);
+            this.MDRBox.Name = "MDRBox";
+            this.MDRBox.Size = new System.Drawing.Size(65, 31);
+            this.MDRBox.TabIndex = 2;
+            this.MDRBox.Text = "";
+            this.MDRBox.TextChanged += new System.EventHandler(this.richTextBox5_TextChanged);
+            // 
+            // MARBox
+            // 
+            this.MARBox.Location = new System.Drawing.Point(788, 233);
+            this.MARBox.Name = "MARBox";
+            this.MARBox.Size = new System.Drawing.Size(65, 31);
+            this.MARBox.TabIndex = 2;
+            this.MARBox.Text = "";
+            this.MARBox.TextChanged += new System.EventHandler(this.richTextBox6_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(808, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "IR";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(808, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "PC";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(804, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "MDR";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(805, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "MAR";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(17, 16);
+            this.richTextBox2.Location = new System.Drawing.Point(57, 67);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(269, 496);
-            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.richTextBox2.Size = new System.Drawing.Size(269, 400);
+            this.richTextBox2.TabIndex = 3;
             this.richTextBox2.Text = "";
-            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged_1);
             // 
-            // groupBox1
+            // timer1
             // 
-            this.groupBox1.Controls.Add(this.richTextBox2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 46);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(300, 528);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Output";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // vScrollBar1
+            // label5
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(334, 57);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(17, 455);
-            this.vScrollBar1.TabIndex = 7;
-            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll_1);
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(805, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "AC";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // ACBox
+            // 
+            this.ACBox.Location = new System.Drawing.Point(788, 285);
+            this.ACBox.Name = "ACBox";
+            this.ACBox.Size = new System.Drawing.Size(65, 31);
+            this.ACBox.TabIndex = 16;
+            this.ACBox.Text = "";
+            this.ACBox.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(704, 641);
-            this.Controls.Add(this.vScrollBar1);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(862, 641);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ACBox);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MARBox);
+            this.Controls.Add(this.MDRBox);
+            this.Controls.Add(this.PCBox);
+            this.Controls.Add(this.IRBox);
+            this.Controls.Add(this.BT_Princ);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem ejecutarToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.ImageList imageList1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BT_Princ;
+        private System.Windows.Forms.RichTextBox IRBox;
+        private System.Windows.Forms.RichTextBox PCBox;
+        private System.Windows.Forms.RichTextBox MDRBox;
+        private System.Windows.Forms.RichTextBox MARBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox ACBox;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Timers;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +13,21 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        static System.Windows.Forms.Timer Timer = new System.Windows.Forms.Timer();
+
+        string lineaOfCode;
+        string lineaOfCodeS;
+        string[] line = new string[1000];
+        int resource;
+        int IR = 0;
+        int PC = 1;
+        int MAR = 0;
+        int MDR = 0;
+        int[] MM = new int[999]; // left direction right data
+        string test;
+        int LengthMemory;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -20,43 +36,183 @@ namespace WindowsFormsApp1
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
+
         }
-        private void private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            
+            lineaOfCode = richTextBox1.Text;
+            lineaOfCodeS = lineaOfCode;
+            lineaOfCode = lineaOfCode.Replace(" ", "");
+            string[] line = lineaOfCode.Split('\n');
+            int pointer = line.Length;
+            for (int i = 0; i < pointer; i++)
+            {
+                MM[123] = 234;
+                MM[234] = 45;
+                
+                IR = i;
+                PC = IR + 1;
+                PCBox.Text = PC.ToString();
+
+                IRBox.Text = IR.ToString();
+                MARBox.Text = MAR.ToString();
+
+                richTextBox2.Text = i.ToString();
+                LengthMemory = line[i].Length;
+                
+
+                if (LengthMemory != 6 && LengthMemory != 3 && LengthMemory != 9)
+                {
+                    // display message error 
+                    richTextBox2.Text = "Error Ccheck line: " + i;
+                }
+                else
+                {
+                    test = line[i].Substring(0, 3);
+
+                    if (LengthMemory == 6) // if has 3
+                    {
+                        if (test.ToUpper() == "ORG") {
+                            MAR = Int32.Parse(line[i].Substring(3, 3));
+                            i = MAR - 1;
+                            
+                        }
+                    }
+                    else if (LengthMemory == 9) // if has 9
+                    {
+                        if (test == "JMP")
+                        {
+                        }
+                        else if (test.ToUpper() == "LDA")
+                        {
+                            test = line[i].Substring(3, 3);
+                            if(test == "ABS")
+                            {  
+                                MAR = Int32.Parse(line[i].Substring(6, 3));
+                                ACBox.Text = MM[MAR].ToString();
+
+                            }
+                            else if (test.ToUpper() == "IND")
+                            {
+
+                                MAR = Int32.Parse(line[i].Substring(6, 3));
+                                
+                                resource = MM[MAR];
+                                ACBox.Text = MM[resource].ToString();
+
+                            }
+                            else if (test.ToUpper() == "REL")
+                            {
+
+                                MAR = Int32.Parse(line[i].Substring(6, 3));
+
+                                resource = MM[MAR];
+                                ACBox.Text = MM[resource].ToString();
+
+                            }
+                        }
+                        else if (test.ToUpper() == "ADD") { }
+                        else if (test.ToUpper() == "STA") { }
+                        else if (test.ToUpper() == "SUB") { }
+                        else if (test.ToUpper() == "CLA") { }
+
+                        else if (test.ToUpper() == "JMP") { }
+                    }
+
+
+
+
+
+
+                    else if(LengthMemory == 3) // if has 3
+                    {
+                        if (test.ToUpper() == "HLT") { }
+                        else if (test.ToUpper() == "NOP") { }
+                        else if (test.ToUpper() == "NEG")
+                        {
+                            richTextBox2.Text = "NEG";
+                        }
+                        
+
+                    }
+                    else  /*message error*/  { }
+
+
+                }
+            }
         }
+        //codigo de prueba
 
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
 
-        }
-
-        private void vScrollBar1_Scroll_1(object sender, ScrollEventArgs e)
-        {
-
-        }
 
         private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void archivoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-          
-        }
+        
 
-        private void editarToolStripMenuItem_Click(object sender, EventArgs e)
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void ejecutarToolStripMenuItem_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged_1(object sender, EventArgs e)
         {
 
         }
